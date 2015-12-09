ee4adcd2e645a8946b0699cc7387e9238009e228
ralenmandao <ralencc@yahoo.com>
2015-12-10 00:14:26 +0800

    added di

---
+++ WORKSPACE4/Sample/src/Person.java
@@ -0,0 +1,9 @@
+import com.ralen.annotation.Bean;
+
+
+public class Person {
+	String message = "aw";
+	public void print(){
+		System.out.println(message);
+	}
+}
---
+++ WORKSPACE4/Sample/src/Sample.java
@@ -0,0 +1,8 @@
+import com.ralen.annotation.Bean;
+
+@Bean("sample")
+public class Sample {
+	public void printMe(){
+		System.out.println("awe");
+	}
+}
